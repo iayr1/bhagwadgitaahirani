1593431509fe8b8be7b0e0a7a2ab3f5f96ec7dbe
Mayur Chaudhari <mayurchaudhari1675@gmail.com>
2026-04-27 18:27:58 +0530

    minor changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.bhagwadgita"
+    namespace = "com.connectonmap.bhagwadgita"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.bhagwadgita"
+        applicationId = "com.connectonmap.bhagwadgita"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/bhagwadgita/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bhagwadgita/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.bhagwadgita
+package com.connectonmap.bhagwadgita
 
 import io.flutter.embedding.android.FlutterActivity
 
